fb510a1b8bcbeb2d13eb79fb53d4c614801813be
null <iam0xsuk@gmail.com>
2024-04-16 19:26:37 +0900

    pwm pico w

--- pwm.c
+++ pwm.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "pico/cyw43_arch.h"
+
+
+void init_led() {
+  if (cyw43_arch_init()) {
+    printf("Wi-Fi init failed");
+  }
+}
+
+void blink() {
+  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+  sleep_ms(250);
+  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+}
 
 int main() {
   stdio_init_all();
-  const uint LED_PIN = 14;
-  gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+  init_led();
+  const uint PIN = 14;
+  gpio_set_function(PIN, GPIO_FUNC_PWM);
 
-  uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
-  uint chan_num = pwm_gpio_to_channel(LED_PIN);
+  uint slice_num = pwm_gpio_to_slice_num(PIN);
+  uint chan_num = pwm_gpio_to_channel(PIN);
   
   int wrap = 99;
   pwm_set_wrap(slice_num, wrap);
@@ -18,6 +33,7 @@ int main() {
   
   int i = 0;
   while (true) {
+    printf("hey\n");
     pwm_set_chan_level(slice_num, PWM_CHAN_A, i);
     sleep_ms(30);
     i++;
